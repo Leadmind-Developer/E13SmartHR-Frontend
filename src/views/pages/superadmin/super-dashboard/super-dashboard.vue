2f45f09733855af2adfd111b9ef989a7715781a7
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2026-04-01 08:33:44 +0100

    Update super-dashboard.vue

--- src/views/pages/superadmin/super-dashboard/super-dashboard.vue
+++ src/views/pages/superadmin/super-dashboard/super-dashboard.vue
@@ -225,6 +225,14 @@ export default {
       donutChart,
     });
 
+    const formatCurrency = (value) => {
+     if (!value) return "$0";
+     return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(value);
+  };
+
     // =========================
     // METHODS
     // =========================
